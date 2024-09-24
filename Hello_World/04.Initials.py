ef4e562e5a0e7ba516f892133f9c8faca97bf077
Fernando Dias <ferpaesdias@gmail.com>
2024-09-24 15:10:19 -0300

    Add File

---
+++ Hello_World/04.Initials.py
@@ -0,0 +1,7 @@
+print("FFFFFFF EEEEEEE RRRRRR")
+print("F       E       R      R")
+print("F       E       R      R")
+print("FFFFF   EEEEE   RRRRRR")
+print("F       E       R   R")
+print("F       E       R    R") 
+print("F       EEEEEEE R     R")
