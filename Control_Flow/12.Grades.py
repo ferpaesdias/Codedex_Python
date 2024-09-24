fec8cd60a32f996c1a5c58a59010a2ea205b15af
Fernando Dias <ferpaesdias@gmail.com>
2024-09-24 16:02:49 -0300

    Add File

---
+++ Control_Flow/12.Grades.py
@@ -0,0 +1,18 @@
+"""Start by creating a variable called grade and give it a value between 
+0-100.
+
+Write a if/else statement for the following:
+
+If grade is greater than or equal to 55, then print "You passed."
+Else, print "You failed."
+After you run the code, change grade's value and rerun it. Do this a few 
+times to make sure it's working as intended.
+
+"""
+
+grade = 70
+
+if grade >= 55:
+    print("You passed.")
+else:
+    print("You failed.")
---
+++ Control_Flow/13.pH_Levels.py
@@ -0,0 +1,24 @@
+"""In chemistry, pH is a scale used to specify the acidity or basicity of a 
+liquid.
+
+Create a ph_levels.py program that checks whether a pH level is basic, acidic, 
+or neutral.
+
+First, create a ph variable and ask the user for a value between 0 and 14.
+
+Write an if, elif, else statement that:
+
+If ph is greater than 7, output "Basic".
+If ph is less than 7, output "Acidic".
+Else, output "Neutral".
+
+"""
+
+ph = int(input("Insira o valor do pH (0-14): "))
+
+if ph > 7:
+    print("Basic")
+elif ph == 7:
+    print("Neutral")
+else:
+    print("Acidic")    
